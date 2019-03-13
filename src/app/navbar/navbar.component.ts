cfbee656237455bfb293756b6ff0ec3ed79917f6
Natalia <nataliadm@ipiranga.com.br>
2019-03-13 20:09:55 -0300

    Adicionando novo componente NAVBAR

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SelectorContext } from '@angular/compiler';
+
+@Component({
+    selector: 'navbar',
+    templateUrl: './navbar.component.html'
+})
+
+export class NavbarComponent {
+
+}
