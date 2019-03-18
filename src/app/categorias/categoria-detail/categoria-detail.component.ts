d172b2bc0af0a49ca96796c38cb6a82b052d7d00
Natalia <nataliadm@ipiranga.com.br>
2019-03-18 00:21:46 -0300

    Passando params para rota CategoriaDetail component

--- src/app/categorias/categoria-detail/categoria-detail.component.ts
+++ src/app/categorias/categoria-detail/categoria-detail.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import 'rxjs/add/operator/switchMap';
 
 import { Categoria } from '../shared/categoria.model';
+import { CategoriaService } from '../shared/categoria.service';
+
 
 @Component({
     selector: 'app-categoria-detail',
     templateUrl: './categoria-detail.component.html',
+    providers: []
 })
 export class CategoriaDetailComponent implements OnInit {
 
     @Input()
     categoria: Categoria;
+    constructor(
+        private categoriaService: CategoriaService,
+        private route: ActivatedRoute
+        ) { }
 
-    constructor() {
+    public ngOnInit() {
+        this.route.params
+        .switchMap((params: Params) => this.categoriaService.getCategoria(+params['id']))
+        .subscribe(categoria => this.categoria = categoria);
      }
-
-    ngOnInit(): void { }
-
 }
